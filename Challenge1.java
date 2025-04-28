c2d8a7117ce1cad910bc72b762cdcae2311e3a3a
AustineIgunza <Austine.igunza@strathmore.edu>
2025-04-28 11:42:57 +0300

    Coding challenge one

---
+++ Challenge1.java
@@ -0,0 +1,16 @@
+//puts message on the screen
+
+public class Challenge1 {
+
+    static class Greeter {
+
+        void greet() {
+            System.out.println("Hi there");
+        }
+    }
+
+    public static void main(String[] args) {
+        (new Greeter()).greet();
+    }
+}
+
