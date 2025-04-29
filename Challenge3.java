dcc4d0f65020d2786f00a143f4381cf65a6394e5
AustineIgunza <Austine.igunza@strathmore.edu>
2025-04-29 13:11:16 +0300

    Coding challenge three

---
+++ Challenge3.java
@@ -0,0 +1,14 @@
+public class Challenge3 {
+    public static void main(String[] args){
+double price = 45.0;
+
+if(price >100){
+    System.out.println("That is very expensive.");
+}else if(price>50){
+    System.out.println("That is affordable");
+        }else{
+    System.out.println("That is very cheap");
+        }
+
+    }
+}
